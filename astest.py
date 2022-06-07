e59a4ca27aa013ce8d7bf5ce9e2e9cc5b2c92db2
cbattieste-godaddy <cbattieste@godaddy.com>
2022-06-07 13:45:20 -0500

    Update test code to fail

--- astest.py
+++ astest.py
@@ -5,10 +5,10 @@ from addsub import add, subtract
 class TestBasicMethods(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(add(2,3), 5)
+        self.assertEqual(add(2,3), 6)
 
     def test_subtract(self):
-        self.assertEqual(subtract(5,3), 2)
+        self.assertEqual(subtract(5,3), 3)
 
 if __name__ == '__main__':
     unittest.main()
